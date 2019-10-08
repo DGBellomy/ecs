7e0afb065b75c249c6c42d13d15399aa54ab175b
Garrett Bellomy <dgbellomy@gmail.com>
2019-10-07 23:57:02 -0500

    working on idea #2 for ecs

---
+++ hello_world/test2.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+enum ComponentType {
+    NUM,
+    STR
+};
+
+class Entity {
+    int m_id;
+
+public:
+    static int Count;
+    //void addComponent(Component& component)
+    //{
+    //}
+};
+
+int Entity::Count = 0;
+
+
+class System {
+public:
+    virtual void update() = 0;
+    static void tick()
+    {
+    }
+};
+
+
+int main()
+{
+    // setup world
+    // add entities
+    // add systems
+    // for (;;)
+    // {
+    //     for sys in systems:
+    //         sys::tick(gameTime);
+    // }
+
+    return 0;
+}
+
+// flexability in adding:
+//      systems
+//      components
+//      entities to the world
