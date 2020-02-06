771c9e15ded87e05fffb806534582b6a5b2f6a21
Garrett Bellomy <dgbellomy@gmail.com>
2020-02-05 18:27:24 -0600

    added comments showing how the interface might be used

--- main.cpp
+++ main.cpp
@@ -7,6 +7,19 @@ Component(Position,
 
 // create a system that handles a "COMPONENT"
 
+void myCallback()
+{
+    // CompList components = ecs::getComponentList<CompType>();
+
+    // TODO: assuming an entity holds ptrs to it's components???
+    // for (component in components) {
+    //   if (component.property > 5) {
+    //     CompType* component = ecs::getComponentRef<CompType>(component1.entityId);
+    //     component->someProperty = value;
+    //   }
+    // }
+}
+
 int main()
 {
     // create components to be assigned to an entity
@@ -16,6 +29,18 @@ int main()
     // Velocity v = { 0, 0 };
 
     // createEntity([...]) with list of components
+
+    // TODO: figure out what the hell I'm going to do
+    // TODO: make typedef (entityID => unsigned int)
+
+    // entityID entityId = ecs::getNewEntityId();
+    // ecs::addComponent<CompType1>(entityId, component1);
+    // ecs::addComponent<CompType2>(entityId, component2);
+    // ecs::registerEntity(entityId);
+
+    // TODO: callback will be a function ptr
+    // TODO: priority will default to 5 - [0:9]???
+    // ecs::registerSystem(callback, priority);
 
     return 0;
 }
